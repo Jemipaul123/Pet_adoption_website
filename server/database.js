0d30a281d31b17525ffd14822ae3315e44ac0817
Arpitha Sudhir <107522016+Jemipaul123@users.noreply.github.com>
2024-07-11 01:19:09 +0530

    Update database.js

--- server/database.js
+++ server/database.js
@@ -2,12 +2,7 @@ const Pool = require('pg').Pool;
 const response = require("express");
 
 const pool = new Pool({
-    user: "postgres",
-    password: "arpsud18",
-    host:"localhost",
-    port: 5432,
-    database: "petadoption",
-
-});
+  connectionString: process.env.POSTGRES_URL,
+})
 
 module.exports = pool;
